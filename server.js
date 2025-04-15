e736d813a6c3747e476ef6270c44ea25f62218bb
Isabella Borin <158210136+isab5@users.noreply.github.com>
2025-04-15 14:45:46 -0300

    feat: create doc swagger

--- server.js
+++ server.js
@@ -4,11 +4,16 @@ const cors = require("cors");
 const wizardRoutes = require("./src/routes/wizardRoutes.js");
 const houseRoutes = require("./src/routes/houseRoutes.js");
 const reportRoutes = require("./src/routes/reportRoutes.js");
+const setupSwagger = require('./src/config/swagger.js'); // Swagger aqui
+const path = require("path");
+
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+setupSwagger(app); // Ativa o Swagger
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+// Prefixo /api para as rotas
 app.use("/api/wizards", wizardRoutes);
 app.use("/api/houses", houseRoutes);
 app.use("/api", reportRoutes);
