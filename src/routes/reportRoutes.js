568f46b2f45ef45aa76a0e3acdd9f10c2b35f002
Thiago Ferreira <thiago.rodrigues@sp.senai.br>
2025-04-22 08:13:46 -0300

    feat: adicionar middleware de validação de chave da API

--- src/routes/reportRoutes.js
+++ src/routes/reportRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-
 const reportController = require("./../controllers/reportController");
+const apiKeyMiddleware = require("./../config/apiKey")
 
+router.use(apiKeyMiddleware);
 router.get("/report/csv", reportController.exportWizardCSV);
 router.get("/report/pdf", reportController.exportWizardPDF);
 
